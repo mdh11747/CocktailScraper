0f905067ba7852c83d5df00c4344ae54c63a47c8
Magnus Hodgson <mdh11747@uga.edu>
2022-02-02 14:37:33 -0500

    randomDrink() method implemented

---
+++ src/main/java/com/example/CocktailAppMain.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import javafx.application.Application;
+
+public class CocktailAppMain {
+
+    public static void main(String[] args) {
+        Application.launch(CocktailApp.class,args);
+    }
+
+}
--- src/main/java/com/example/CocktailScraper.java
+++ src/main/java/com/example/CocktailScraper.java
@@ -11,44 +11,47 @@ import org.jsoup.select.Evaluator.Class;
 
 public class CocktailScraper 
 {
-    static FileWriter cocktails;
+    private FileWriter cocktails;
+    private int drinkNumber;
+    private int spiritNumber = 1;
 
     public static void main( String[] args ) throws IOException
     {
+        CocktailScraper scraper = new CocktailScraper();
         Document doc = Jsoup.connect("https://www.liquor.com/cocktail-by-spirit-4779438").get();
         Elements spirits = doc.select(new Class("truncated-list__item"));
         for (Element spirit : spirits) {
-            scrape(spirit.child(0).attr("href"));
+            scraper.scrape(spirit.child(0).attr("href"));
         }
     }
 
-    public static void scrape(String sUrl) {
+    public void scrape(String sUrl) {
         try {
             cocktails = new FileWriter("Cocktails.txt",true);
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+        drinkNumber = 1;
         Document doc;
         try {
             doc = Jsoup.connect(sUrl).get();
             String spiritName = doc.select(new Class("heading__title")).get(0).text();
-            cocktails.write("Spirit: " + spiritName + "\n");
+            cocktails.write("Spirit" + (spiritNumber++) + ": " + spiritName + "\n");
                 Elements drinks = doc.select(new Class("comp card-list__item mntl-block"));
                 for (Element drink : drinks) {
                     doc = Jsoup.connect(drink.child(0).attr("href")).get();
                     String title = doc.select(new Class("heading__title")).get(0).text();
                     Elements ingredients = doc.select(new Class("simple-list__item js-checkbox-trigger ingredient text-passage"));
                     Elements instructions = doc.select("#mntl-sc-block_3-0");
-                    Element image = doc.select(new Class("figure__media js-figure-media figure__media--square ")).get(0);
-                    System.out.println(image.child(0).absUrl("src"));
-                    /*if (ingredients.size() == 0) {
+                    Element img = doc.select("img[src]").first();
+                    if (ingredients.size() == 0) {
                         ingredients = doc.select(new Class("structured-ingredients__list-item"));
                     }
-                    if (ingredients.size() > 0) {
-                        cocktails.write("Title: " + title + "\n");
-                        for (Element ingredient : ingredients) {
-                            cocktails.write("Ingredients: " + ingredient.text() + "\n");
+                    if (ingredients.size() > 0 && !instructions.get(0).child(0).text().contains("Serves")) {
+                        cocktails.write("Title" + (drinkNumber++) + ": " + title + "\n");
+                        for (int i = 0; i < ingredients.size(); i++) {
+                            cocktails.write("Ingredients" + (i+1) + ": " + ingredients.get(i).text() + "\n");
                         }
                         for (int i = 0; i < instructions.size(); i++) {
 
@@ -56,12 +59,17 @@ public class CocktailScraper
                                 cocktails.write("Instructions" + (n+1) + ": " + instructions.get(i).child(n).text() + "\n");
                             }
                         }
+                        if (img.absUrl("src").contains(title.toLowerCase().substring(0,3))) {
+                            cocktails.write("Image: " + img.absUrl("src") + "\n");
+                        } else {
+                            cocktails.write("Image: " + doc.select("img[src]").get(1).absUrl("src") + "\n");
+                        }
 
-                    }*/
+                    }
                 }
 
                 cocktails.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
-                                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 cocktails.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
